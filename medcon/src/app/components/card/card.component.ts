e761872130d359503e0513e41eefa21be0fea46e
Gekyume Serna <contatogekyume@gmail.com>
2024-12-04 12:09:03 -0300

    exibe nome do paciente no card component

--- medcon/src/app/components/card/card.component.ts
+++ medcon/src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, DestroyRef} from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
 import { AppointmentsService } from '../../modules/appointments/services/appointments.service';
 import { RouterLink } from '@angular/router';
 import { Appointment } from '../../modules/appointments/models/appointments.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
-
+import { User } from '../../modules/auth/models/user.model';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -13,88 +14,86 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent{
+export class CardComponent {
   appointments: Appointment[] = []
+  userName: string = ''
+  users: User[] = []
 
   constructor(
-    private appointmentsService:AppointmentsService,
+    private appointmentsService: AppointmentsService,
     private destroyRef: DestroyRef,
-  ){}
+  ) { }
 
-  ngOnInit():void{
-    this.getAppointments();
+
+  ngOnInit(): void {
+    this.loadData();
   }
 
-  getAppointments():void{
-    this.appointmentsService
-      .getAppointments()
-      .pipe(takeUntilDestroyed(this.destroyRef))
+  loadData(): void {
+    forkJoin({
+      users: this.appointmentsService.getUsers(),
+      appointments: this.appointmentsService.getAppointments(),
+    }).pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
-        next: (res) => {
-          this.appointments = res;
+        next: ({ users, appointments }) => {
+          this.users = users;
+          this.appointments = appointments;
+          console.log('Dados carregados:', { users, appointments });
         },
         error: (err) => {
-          console.log(err);
+          console.error('Erro ao carregar os dados:', err);
+          alert('Erro ao carregar os dados. Tente novamente mais tarde.');
         },
       });
   }
 
-  getAppointmentsById(id: string): Appointment | void{
-    this.appointmentsService
-      .getAppointmentsById(id)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe({
-        next: (res: Appointment) => {
-          return res;
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
+
+  deleteAppointment(id: string): void {
+    if (confirm("Deseja deletar o agendamento?")) {
+      this.appointmentsService
+        .deleteAppointments(id)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          complete: () => {
+            this.loadData();
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        });
+    }
   }
 
-  deleteAppointment(id:string):void{
-    alert("Deseja Deletar o agendamento?")
-    this.appointmentsService
-    .deleteAppointments(id)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe({
-      complete:()=>{
-        this.getAppointments()
-      },
-      error:(error) =>{
-        console.log(error);
-      },
-    })
+  doneAppointment(appointment: Appointment): void {
+    if (confirm("Deseja marcar o agendamento como concluído?")) {
+      this.appointmentsService
+        .doneAppointment(appointment)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          complete: () => {
+            this.loadData()
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        })
+    }
+  }
+  
+  cancelAppointment(appointment: Appointment): void {
+    if (confirm("Deseja cancelar o agendamento ?")) {
+      this.appointmentsService
+        .cancelAppointment(appointment)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          complete: () => {
+            this.loadData()
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        })
+    }
   }
 
-  doneAppointment(appointment:Appointment):void{
-    alert("Deseja Marcar o agendamento como feito?")
-    this.appointmentsService
-    .doneAppointment(appointment)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe({
-      complete:()=>{
-        this.getAppointments()
-      },
-      error:(error) =>{
-        console.log(error);
-      },
-    })
-  }
-
-  cancelAppointment(appointment:Appointment):void{
-    alert("Deseja cancelar o agendamento?")
-    this.appointmentsService
-    .cancelAppointment(appointment)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe({
-      complete:()=>{
-        this.getAppointments()
-      },
-      error:(error) =>{
-        console.log(error);
-      },
-    })
-  }
 }
